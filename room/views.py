7c6d1f046e4983d029423576f24e43ee5f21c8a7
Abhijeet Gupta <abhijeetg40@gmail.com>
2024-08-22 13:47:58 +0530

    final commit

--- room/views.py
+++ room/views.py
@@ -6,67 +6,10 @@ from .serializers import (
     DiscountSerializer, DiscountRoomRateSerializer
 )
 from django.db.models import Min, F
+from utils.utils import CustomViewSet
 from rest_framework.filters import SearchFilter
 
-class CustomViewSet(viewsets.ModelViewSet):
-    def create(self, request, *args, **kwargs):
-        serializer = self.get_serializer(data=request.data)
-        if serializer.is_valid():
-            self.perform_create(serializer)
-            return Response({
-                "status": True,
-                "message": "Data created successfully",
-                "data": serializer.data
-            }, status=status.HTTP_201_CREATED)
-        return Response({
-            "status": False,
-            "message": "Missing fields",
-            "data": serializer.errors
-        }, status=status.HTTP_400_BAD_REQUEST)
 
-    def update(self, request, *args, **kwargs):
-        partial = kwargs.pop('partial', False)
-        instance = self.get_object()
-        serializer = self.get_serializer(instance, data=request.data, partial=partial)
-        if serializer.is_valid():
-            self.perform_update(serializer)
-            return Response({
-                "status": True,
-                "message": "Data updated successfully",
-                "data": serializer.data
-            }, status=status.HTTP_200_OK)
-        return Response({
-            "status": False,
-            "message": "Missing fields",
-            "data": serializer.errors
-        }, status=status.HTTP_400_BAD_REQUEST)
-
-    def retrieve(self, request, *args, **kwargs):
-        instance = self.get_object()
-        serializer = self.get_serializer(instance)
-        return Response({
-            "status": True,
-            "message": "Data fetched successfully",
-            "data": serializer.data
-        }, status=status.HTTP_200_OK)
-
-    def list(self, request, *args, **kwargs):
-        queryset = self.filter_queryset(self.get_queryset())
-        serializer = self.get_serializer(queryset, many=True)
-        return Response({
-            "status": True,
-            "message": "Data fetched successfully",
-            "data": serializer.data
-        }, status=status.HTTP_200_OK)
-
-    def destroy(self, request, *args, **kwargs):
-        instance = self.get_object()
-        self.perform_destroy(instance)
-        return Response({
-            "status": True,
-            "message": "Data deleted successfully",
-            "data": {}
-        }, status=status.HTTP_204_NO_CONTENT)
 
 class RoomRateViewSet(CustomViewSet):
     queryset = RoomRate.objects.all()
--- utils/utils.py
+++ utils/utils.py
@@ -1,6 +1,7 @@
 from rest_framework.views import exception_handler
 from rest_framework.response import Response
-from rest_framework import status
+from rest_framework import status,viewsets
+ 
 
 def custom_exception_handler(exc, context):
     response = exception_handler(exc, context)
@@ -17,3 +18,64 @@ def custom_exception_handler(exc, context):
         "message": "Something went wrong",
         "data": {}
     }, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+
+class CustomViewSet(viewsets.ModelViewSet):
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        if serializer.is_valid():
+            self.perform_create(serializer)
+            return Response({
+                "status": True,
+                "message": "Data created successfully",
+                "data": serializer.data
+            }, status=status.HTTP_201_CREATED)
+        return Response({
+            "status": False,
+            "message": "Missing fields",
+            "data": serializer.errors
+        }, status=status.HTTP_400_BAD_REQUEST)
+
+    def update(self, request, *args, **kwargs):
+        partial = kwargs.pop('partial', False)
+        instance = self.get_object()
+        serializer = self.get_serializer(instance, data=request.data, partial=partial)
+        if serializer.is_valid():
+            self.perform_update(serializer)
+            return Response({
+                "status": True,
+                "message": "Data updated successfully",
+                "data": serializer.data
+            }, status=status.HTTP_200_OK)
+        return Response({
+            "status": False,
+            "message": "Missing fields",
+            "data": serializer.errors
+        }, status=status.HTTP_400_BAD_REQUEST)
+
+    def retrieve(self, request, *args, **kwargs):
+        instance = self.get_object()
+        serializer = self.get_serializer(instance)
+        return Response({
+            "status": True,
+            "message": "Data fetched successfully",
+            "data": serializer.data
+        }, status=status.HTTP_200_OK)
+
+    def list(self, request, *args, **kwargs):
+        queryset = self.filter_queryset(self.get_queryset())
+        serializer = self.get_serializer(queryset, many=True)
+        return Response({
+            "status": True,
+            "message": "Data fetched successfully",
+            "data": serializer.data
+        }, status=status.HTTP_200_OK)
+
+    def destroy(self, request, *args, **kwargs):
+        instance = self.get_object()
+        self.perform_destroy(instance)
+        return Response({
+            "status": True,
+            "message": "Data deleted successfully",
+            "data": {}
+        }, status=status.HTTP_204_NO_CONTENT)
